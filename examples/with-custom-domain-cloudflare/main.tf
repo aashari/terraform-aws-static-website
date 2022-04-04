7e5352f7b26bd77f8142a13f4f8a3babe6d3a7eb
aashari <andi@ashari.me>
2022-04-04 08:46:48 +0000

    re-format (fmt) examples main.tf

--- examples/with-custom-domain-cloudflare/main.tf
+++ examples/with-custom-domain-cloudflare/main.tf
@@ -1,12 +1,8 @@
 module "static-website" {
-  source = "../../"
-
-  name = "ashari.me"
-
+  source                 = "../../"
+  name                   = "ashari.me"
   custom_domain_provider = "CLOUDFLARE"
   custom_domain_records  = ["@", "www"]
   custom_domain_zone_id  = var.custom_domain_zone_id
-
-  cloudflare_api_token      = var.cloudflare_api_token
-
+  cloudflare_api_token   = var.cloudflare_api_token
 }
--- examples/with-custom-domain-route53/main.tf
+++ examples/with-custom-domain-route53/main.tf
@@ -1,10 +1,7 @@
 module "static-website" {
-  source = "../../"
-
-  name = "ashari.tech"
-
+  source                 = "../../"
+  name                   = "ashari.tech"
   custom_domain_provider = "ROUTE53"
   custom_domain_records  = ["@", "www"]
   custom_domain_zone_id  = var.custom_domain_zone_id
-
 }
